778313734dacfb034bddf314594bf5ad1b8ef827
Colin Bell <colin@dogoodercomics.co.uk>
2019-02-18 14:06:41 +0000

    we have a map

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template lang="html">
-
+    <div id="mapContainer"></div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      map: null
+    }
+  },
+  mounted(){
+    this.map = L.map('mapContainer').setView([0,0], 2).addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"))
+  }
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="css">
+#mapContainer{
+  width:500px;
+  height:500px;
+}
 </style>
