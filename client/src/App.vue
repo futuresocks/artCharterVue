f943781d6ed25113b4ced898cc451e6597de6ad3
Colin Bell <colin@dogoodercomics.co.uk>
2019-02-18 23:45:52 +0000

    adds muralInfo to app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,13 @@
 <template lang="html">
   <div>
     <mapView :murals="murals"/>
+    <muralInfo :mural="selectedMural"/>
   </div>
 </template>
 
 <script>
 import MapView from "./components/MapView.vue";
+import MuralInfo from "./components/MuralInfo.vue";
 import { eventBus } from './main.js';
 
 export default {
@@ -16,14 +18,15 @@ export default {
     }
   },
   components: {
-    'mapView': MapView
+    'mapView': MapView,
+    'muralInfo': MuralInfo
   },
   mounted(){
-      fetch("http://localhost:3000/api/murals")
-      .then(response => response.json())
-      .then(murals => this.murals = murals)
+    fetch("http://localhost:3000/api/murals")
+    .then(response => response.json())
+    .then(murals => this.murals = murals)
 
-      eventBus.$on('marker-clicked', index => this.selectedMural = this.murals[index])
+    eventBus.$on('marker-clicked', index => this.selectedMural = this.murals[index])
   }
 }
 </script>
