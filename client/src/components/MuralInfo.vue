511e8d3e851ae128776cf5481f87b5e5c4d35818
Colin Bell <colin@dogoodercomics.co.uk>
2019-02-18 23:45:21 +0000

    adds muralInfo

---
+++ client/src/components/MuralInfo.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <div v-if="mural">
+      <h1>{{mural.title}}</h1>
+      <h2>{{mural.artist}}</h2>
+      <muralImage :imgTag="mural.imageTags"/>
+    </div>
+</template>
+
+<script>
+import MuralImage from './MuralImage.vue';
+
+export default {
+  name: 'muralInfo',
+  props: ['mural'],
+  components: {
+    'muralImage': MuralImage
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
